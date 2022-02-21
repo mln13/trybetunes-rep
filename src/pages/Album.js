da4d833f33a01e235ff6276bf4c6db55a2d7df8d
Matheus <kamaitatsu@gmail.com>
2022-02-21 11:32:07 -0300

    recommit

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,53 +1,58 @@
-import propTypes from 'prop-types';
+// import propTypes from 'prop-types';
 import React from 'react';
 import Header from '../components/Header';
-import getMusics from '../services/musicsAPI';
+// import getMusics from '../services/musicsAPI';
+// import Loading from './Loading';
 
 class Album extends React.Component {
-  constructor() {
-    super();
-    this.musicsFromAlbum = this.musicsFromAlbum.bind(this);
+  // constructor() {
+  //   super();
+  //   this.musicsFromAlbum = this.musicsFromAlbum.bind(this);
 
-    this.state = {
-      musics: [],
-    };
-  }
+  //   this.state = {
+  //     musics: [],
+  //     loading: true,
+  //   };
+  // }
 
-  componentDidMount() {
-    this.musicsFromAlbum();
-  }
+  // componentDidMount() {
+  //   this.musicsFromAlbum();
+  // }
 
-  async musicsFromAlbum() {
-    const { match } = this.props;
-    const musics = await getMusics(match.params.id);
-    this.setState({
-      musics,
-    });
-  }
+  // async musicsFromAlbum() {
+  //   const { match } = this.props;
+  //   const musics = await getMusics(match.params.id);
+  //   this.setState({
+  //     musics,
+  //     loading: false,
+  //   });
+  // }
 
   render() {
-    const { musics } = this.state;
+    // const { musics, loading } = this.state;
     return (
+      // loading ? <Loading />
+      //   : (
       <div data-testid="page-album">
         <Header />
-        <div data-testid="artist-name">
+        {/* <div data-testid="artist-name">
           <h3>{musics[0].artistName}</h3>
         </div>
         <div data-testid="album-name">
           <h4>{musics[0].collectionName}</h4>
         </div>
-        {musics.map((music) => <li key={ music }>{music}</li>)}
+        {musics.map((music) => <li key={ music }>{music}</li>)} */}
       </div>
     );
   }
 }
 
-Album.propTypes = {
-  match: propTypes.objectOf({
-    params: propTypes.objectOf({
-      id: propTypes.string,
-    }),
-  }).isRequired,
-};
+// Album.propTypes = {
+//   match: propTypes.objectOf({
+//     params: propTypes.objectOf({
+//       id: propTypes.string,
+//     }),
+//   }).isRequired,
+// };
 
 export default Album;
